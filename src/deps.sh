8c70840a416997d4e1493272146eb38bf23da63c
stefanprodan <stefan.prodan@gmail.com>
2019-10-19 10:11:52 +0300

    Use latest yq version

--- src/deps.sh
+++ src/deps.sh
@@ -6,7 +6,7 @@ curl -sL https://storage.googleapis.com/kubernetes-release/release/$(curl -s htt
 
 curl -sL https://github.com/instrumenta/kubeval/releases/latest/download/kubeval-linux-amd64.tar.gz | tar xz && mv kubeval /bin/kubeval
 
-curl -sL https://github.com/mikefarah/yq/releases/download/2.4.0/yq_linux_amd64 -o /usr/local/bin/yq && chmod +x /usr/local/bin/yq
+curl -sL https://github.com/mikefarah/yq/releases/latest/download/yq_linux_amd64 -o /usr/local/bin/yq && chmod +x /usr/local/bin/yq
 
 curl -sL https://storage.googleapis.com/kubernetes-helm/helm-v2.15.0-linux-amd64.tar.gz | tar xz && mv linux-amd64/helm /bin/helm && rm -rf linux-amd64
 
