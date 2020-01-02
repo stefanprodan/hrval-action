afcd6018329773324b3b57240edacfe866cc9c23
Ed Shelton <edmund.shelton@imaginecurve.com>
2020-01-02 15:56:19 +0000

    remove tmpdir

--- src/hrval.sh
+++ src/hrval.sh
@@ -43,10 +43,14 @@ function clone {
   GIT_REF=$(yq r ${1} spec.chart.ref)
   CHART_PATH=$(yq r ${1} spec.chart.path)
   cd ${2}
-  git init -q
-  git remote add origin ${GIT_REPO}
-  git fetch -q origin
-  git checkout -q ${GIT_REF}
+  if [[ -d ".git" ]]; then
+    echo "Repo ${2} already cloned, skipping"
+  else
+    git init -q
+    git remote add origin ${GIT_REPO}
+    git fetch -q origin
+    git checkout -q ${GIT_REF}
+  fi
   cd ${ORIGIN}
   echo ${2}/${CHART_PATH}
 }
@@ -57,15 +61,17 @@ function validate {
     exit 1
   fi
 
-  TMPDIR=$(mktemp -d)
   CHART_PATH=$(yq r ${HELM_RELEASE} spec.chart.path)
+  CHART_REF=$(yq r ${HELM_RELEASE} spec.chart.ref)
+  NONTMPDIR="/tmp/hrval/${CHART_PATH}/${CHART_REF}"
+  mkdir -p "${NONTMPDIR}"
 
   if [[ "${CHART_PATH}" == "null" ]]; then
-    echo "Downloading to ${TMPDIR}"
-    CHART_DIR=$(download ${HELM_RELEASE} ${TMPDIR}| tail -n1)
+    echo "Downloading to ${NONTMPDIR}"
+    CHART_DIR=$(download ${HELM_RELEASE} ${NONTMPDIR}| tail -n1)
   else
-    echo "Cloning to ${TMPDIR}"
-    CHART_DIR=$(clone ${HELM_RELEASE} ${TMPDIR}| tail -n1)
+    echo "Cloning to ${NONTMPDIR}"
+    CHART_DIR=$(clone ${HELM_RELEASE} ${NONTMPDIR}| tail -n1)
   fi
 
   HELM_RELEASE_NAME=$(yq r ${HELM_RELEASE} metadata.name)
@@ -73,13 +79,13 @@ function validate {
 
   if [[ ${IGNORE_VALUES} == "true" ]]; then
     echo "Ingnoring Helm release values"
-    echo "" > ${TMPDIR}/${HELM_RELEASE_NAME}.values.yaml
+    echo "" > ${NONTMPDIR}/${HELM_RELEASE_NAME}.values.yaml
   else
-    echo "Extracting values to ${TMPDIR}/${HELM_RELEASE_NAME}.values.yaml"
-    yq r ${HELM_RELEASE} spec.values > ${TMPDIR}/${HELM_RELEASE_NAME}.values.yaml
+    echo "Extracting values to ${NONTMPDIR}/${HELM_RELEASE_NAME}.values.yaml"
+    yq r ${HELM_RELEASE} spec.values > ${NONTMPDIR}/${HELM_RELEASE_NAME}.values.yaml
   fi
 
-  echo "Writing Helm release to ${TMPDIR}/${HELM_RELEASE_NAME}.release.yaml"
+  echo "Writing Helm release to ${NONTMPDIR}/${HELM_RELEASE_NAME}.release.yaml"
   if [[ ${HELM_VER} == "v3" ]]; then
     # Helm v3 bug: https://github.com/helm/helm/issues/6416
 #    if [[ "${CHART_PATH}" != "null" ]]; then
@@ -88,7 +94,7 @@ function validate {
     helmv3 template ${HELM_RELEASE_NAME} ${CHART_DIR} \
       --namespace ${HELM_RELEASE_NAMESPACE} \
       --skip-crds=true \
-      -f ${TMPDIR}/${HELM_RELEASE_NAME}.values.yaml > ${TMPDIR}/${HELM_RELEASE_NAME}.release.yaml
+      -f ${NONTMPDIR}/${HELM_RELEASE_NAME}.values.yaml > ${NONTMPDIR}/${HELM_RELEASE_NAME}.release.yaml
   else
     if [[ "${CHART_PATH}" != "null" ]]; then
       helm dependency build ${CHART_DIR}
@@ -96,11 +102,11 @@ function validate {
     helm template ${CHART_DIR} \
       --name ${HELM_RELEASE_NAME} \
       --namespace ${HELM_RELEASE_NAMESPACE} \
-      -f ${TMPDIR}/${HELM_RELEASE_NAME}.values.yaml > ${TMPDIR}/${HELM_RELEASE_NAME}.release.yaml
+      -f ${NONTMPDIR}/${HELM_RELEASE_NAME}.values.yaml > ${NONTMPDIR}/${HELM_RELEASE_NAME}.release.yaml
   fi
 
   echo "Validating Helm release ${HELM_RELEASE_NAME}.${HELM_RELEASE_NAMESPACE} against Kubernetes ${KUBE_VER}"
-  kubeval --strict --ignore-missing-schemas --kubernetes-version ${KUBE_VER} ${TMPDIR}/${HELM_RELEASE_NAME}.release.yaml
+  kubeval --strict --ignore-missing-schemas --kubernetes-version ${KUBE_VER} ${NONTMPDIR}/${HELM_RELEASE_NAME}.release.yaml
 }
 
 validate
