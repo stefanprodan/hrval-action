dba0f96c918e15998c4ce61f0a60623191b0d5cd
Justin Patriquin <justinpatriquin@gmail.com>
2020-02-10 10:06:29 -0400

    The latest version of yq broke this script
    yq now returns an empty string instead of the string "null" when it
    can't find a value in the yaml file. This PR also pins the yq version
    that there won't be any more breaking changes.

--- src/deps.sh
+++ src/deps.sh
@@ -6,7 +6,7 @@ curl -sL https://storage.googleapis.com/kubernetes-release/release/$(curl -s htt
 
 curl -sL https://github.com/instrumenta/kubeval/releases/latest/download/kubeval-linux-amd64.tar.gz | tar xz && mv kubeval /bin/kubeval
 
-curl -sL https://github.com/mikefarah/yq/releases/latest/download/yq_linux_amd64 -o /usr/local/bin/yq && chmod +x /usr/local/bin/yq
+curl -sL https://github.com/mikefarah/yq/releases/download/3.1.0/yq_linux_amd64 -o /usr/local/bin/yq && chmod +x /usr/local/bin/yq
 
 curl -sSL https://get.helm.sh/helm-v2.16.1-linux-amd64.tar.gz | tar xz && mv linux-amd64/helm /bin/helm && rm -rf linux-amd64
 helm init --client-only --kubeconfig=$HOME/.kube/kubeconfig
