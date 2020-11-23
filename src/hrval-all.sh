b043760df1c23d51d097d02dfda913f70b5d38cb
Worawat Wijarn <worawat.wijarn@gmail.com>
2020-11-23 14:30:38 +0700

    Support comma separated list of directories

--- src/hrval-all.sh
+++ src/hrval-all.sh
@@ -2,7 +2,7 @@
 
 set -o errexit
 
-DIR=${1}
+DIRS=${1}
 IGNORE_VALUES=${2-false}
 KUBE_VER=${3-master}
 HELM_VER=${4-v2}
@@ -38,45 +38,54 @@ if [[ ${GCS_REPO} == true ]]; then
     # helm repo update
 fi
 
-# If the path provided is actually a file, just run hrval against this one file
-if test -f "${DIR}"; then
-  ${HRVAL} "${DIR}" "${IGNORE_VALUES}" "${KUBE_VER}" "${HELM_VER}" "${CACHEDIR}"
-  exit 0
-fi
-
-# If the path provided is not a directory, print error message and exit
-if [ ! -d "$DIR" ]; then
-  echo "\"${DIR}\" directory not found!"
-  exit 1
-fi
-
-function isHelmRelease {
-  KIND=$(yq r "${1}" kind)
-  if [[ ${KIND} == "HelmRelease" ]]; then
-      echo true
-  else
-    echo false
+function validate {
+  DIR=${1}
+  # If the path provided is actually a file, just run hrval against this one file
+  if test -f "${DIR}"; then
+    ${HRVAL} "${DIR}" "${IGNORE_VALUES}" "${KUBE_VER}" "${HELM_VER}" "${CACHEDIR}"
+    exit 0
   fi
+
+  # If the path provided is not a directory, print error message and exit
+  if [ ! -d "$DIR" ]; then
+    echo "\"${DIR}\" directory not found!"
+    exit 1
+  fi
+
+  function isHelmRelease {
+    KIND=$(yq r "${1}" kind)
+    if [[ ${KIND} == "HelmRelease" ]]; then
+        echo true
+    else
+      echo false
+    fi
+  }
+
+  # Find yaml files in directory recursively
+  TMPDIR=$(mktemp -d)
+  kustomize build ${DIR} -o ${TMPDIR}
+
+  FILES_TESTED=0
+  declare -a FOUND_FILES=()
+  while read -r file; do
+      FOUND_FILES+=( "$file" )
+  done < <(find "${TMPDIR}" -type f -name '*.yaml' -o -name '*.yml')
+
+  for f in "${FOUND_FILES[@]}"; do
+    if [[ $(isHelmRelease "${f}") == "true" ]]; then
+      ${HRVAL} "${f}" "${IGNORE_VALUES}" "${KUBE_VER}" "${HELM_VER}" "${CACHEDIR}"
+      FILES_TESTED=$(( FILES_TESTED+1 ))
+    else
+      echo "Ignoring ${f} not a HelmRelease"
+    fi
+  done
+
+  # This will set the GitHub actions output 'numFilesTested'
+  echo "::set-output name=numFilesTested::${FILES_TESTED}"
 }
 
-# Find yaml files in directory recursively
-TMPDIR=$(mktemp -d)
-kustomize build ${DIR} -o ${TMPDIR}
-
-FILES_TESTED=0
-declare -a FOUND_FILES=()
-while read -r file; do
-    FOUND_FILES+=( "$file" )
-done < <(find "${TMPDIR}" -type f -name '*.yaml' -o -name '*.yml')
-
-for f in "${FOUND_FILES[@]}"; do
-  if [[ $(isHelmRelease "${f}") == "true" ]]; then
-    ${HRVAL} "${f}" "${IGNORE_VALUES}" "${KUBE_VER}" "${HELM_VER}" "${CACHEDIR}"
-    FILES_TESTED=$(( FILES_TESTED+1 ))
-  else
-    echo "Ignoring ${f} not a HelmRelease"
-  fi
+IFS=',' read -r -a array <<< "$DIRS"
+for dir in "${array[@]}"; do
+    echo "Validating $dir"
+    validate "${dir}"
 done
-
-# This will set the GitHub actions output 'numFilesTested'
-echo "::set-output name=numFilesTested::${FILES_TESTED}"
